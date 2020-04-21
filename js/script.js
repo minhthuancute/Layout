46df5f760635f2983cca249f0dee9a947277e9d2
minhthuancute <minhthuan140299@gmail.com>
2020-04-22 02:20:49 +0700

    adddd

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function () {
+   $('#landing').owlCarousel({
+      loop: true,
+      items: 1,
+      autoplay: true,
+      autoPlaySpeed: 5000,
+      autoPlayTimeout: 5000,
+      autoplayHoverPause: true,
+   })
+
+   $('#new-products').owlCarousel({
+      loop: true,
+      items: 4,
+      autoplay: true,
+      autoPlaySpeed: 5000,
+      autoPlayTimeout: 5000,
+      autoplayHoverPause: true,
+   })
+});
